cf4ccc5164a5650eda13f618db1763c9f2c9cd34
Nikita Dudnik <nikdudnik@gmail.com>
2012-07-02 19:22:55 +0700

    Update repository.

--- Formula/fishfish.rb
+++ Formula/fishfish.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Fishfish < Formula
   homepage 'http://ridiculousfish.com/shell/index.html'
-  head 'https://github.com/fish-shell/fish-shell.git'
+  head 'git://github.com/Nek/homebrew-fishfish.git'
   url 'https://github.com/fish-shell/fish-shell/zipball/master'
   md5 'a8a222e171f7ea6bf2c67a1770141c6c'
   version '1.23.1'
